6cf2d34c69517642c2e798c485673404b3d0f768
Guilherme Cesário <guilhermecesario1808@gmail.com>
2026-02-23 11:17:00 -0300

    redirect da tela inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useAuth } from "../contexts/AuthContexts";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const { loading, user } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!loading && !user) {
-      router.push("/login");
-    }
-  }, [loading, user]);
-
-  return null;
+  redirect("/dashboard");
 }
