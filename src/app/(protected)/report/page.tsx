9337d2a7e5b280cc8bd7866fdb5248fb719b2f66
Guilherme Cesário <guilhermecesario1808@gmail.com>
2026-09-25 14:33:23 -0300

    mudança nas cores da tela de extrato; analytics do vercel

--- src/app/(protected)/report/page.tsx
+++ src/app/(protected)/report/page.tsx
@@ -1,18 +1,20 @@
-"use client";
-import { Trash2 } from "lucide-react";
-import { useTransactionContext } from "@/src//hooks/useTransaction";
+'use client';
+import { Trash2 } from 'lucide-react';
+import { useTransactionContext } from '@/src//hooks/useTransaction';
 
 export default function Report() {
   const { transactions, deleteTransaction } = useTransactionContext();
 
   return (
-    <div className="p-6 w-full max-w-3xl mx-auto">
+    <div className="p-6 w-full max-w-3xl mx-auto justify-center">
       <h1 className="text-2xl font-bold mb-4">Extrato</h1>
-      <div className="bg-white p-6 rounded-xl shadow">
+      <div className="bg-black-forest-active p-6 rounded-xl shadow">
         {transactions && transactions.length === 0 ? (
-          <p className="text-gray-500">Nenhuma transação registrada.</p>
+          <p className="text-foreground-secondary">
+            Nenhuma transação registrada.
+          </p>
         ) : (
-          <ul className="divide-y divide-gray-200">
+          <ul className="divide-y divide-sunlit-clay-secondary">
             {transactions &&
               transactions
                 .sort(
@@ -25,32 +27,33 @@ export default function Report() {
                     className="py-3 flex justify-between items-center"
                   >
                     <div>
-                      <p className="font-medium text-gray-800">
+                      <p className="font-medium text-gray-100">
                         {t.description}
                       </p>
-                      <p className="text-sm text-gray-500">
-                        {t.category} •{" "}
-                        {new Date(t.date).toLocaleDateString("pt-BR")}
+                      <p className="text-sm text-gray-300">
+                        {t.category}{' '}
+                        <span className="text-sunlit-clay-secondary">•</span>{' '}
+                        {new Date(t.date).toLocaleDateString('pt-BR')}
                       </p>
                     </div>
                     <div className="flex items-center gap-4">
                       <p
                         className={`font-semibold ${
-                          t.type === "entrada"
-                            ? "text-green-600"
-                            : "text-red-600"
+                          t.type === 'entrada'
+                            ? 'text-olive-leaf-secondary'
+                            : 'text-danger-hover'
                         }`}
                       >
-                        {t.type === "entrada" ? "+" : "-"}{" "}
-                        {t.amount.toLocaleString("pt-BR", {
-                          style: "currency",
-                          currency: "BRL",
+                        {t.type === 'entrada' ? '+' : '-'}{' '}
+                        {t.amount.toLocaleString('pt-BR', {
+                          style: 'currency',
+                          currency: 'BRL',
                         })}
                       </p>
 
                       <button
                         onClick={() => deleteTransaction(String(t.id))}
-                        className="p-2 rounded hover:bg-red-100 text-red-600 transition"
+                        className="p-2 rounded hover:bg-danger-hover text-danger transition"
                         title="Excluir transação"
                       >
                         <Trash2 size={18} strokeWidth={2} />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,25 @@
-import React from "react";
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { TransactionProvider } from "../contexts/TransactionContext";
-import { AuthProvider } from "../contexts/AuthContexts";
+import React from 'react';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { TransactionProvider } from '../contexts/TransactionContext';
+import { AuthProvider } from '../contexts/AuthContexts';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Finance Manager",
-  description: "Gerenciador financeiro pessoal",
+  title: 'Finance Manager',
+  description: 'Gerenciador financeiro pessoal',
 };
 
 export default function RootLayout({
@@ -34,6 +36,8 @@ export default function RootLayout({
           <TransactionProvider>{children}</TransactionProvider>
         </AuthProvider>
       </body>
+      <SpeedInsights />
+      <Analytics />
     </html>
   );
 }
